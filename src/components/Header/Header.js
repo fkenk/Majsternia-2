ceaea5cbc6d8470213334bb4e28ae400e106ae61
fkenk <fkenkandfkenk@gmail.com>
2017-06-04 18:27:23 +0300

    menu mobile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,8 +6,11 @@ import s from './Header.css';
 import s2 from './HeaderSecondary.css';
 import Link from '../Link';
 import logo from './Group.svg'
+import cross from './Cross.svg'
 import MyLanguageSwitcher from '../MyLanguageSwitcher'
 import Info from '../Info'
+import Menu from 'react-burger-menu/lib/menus/slide'
+
 
 const messages = defineMessages({
   brand: {
@@ -27,16 +30,65 @@ const messages = defineMessages({
   },
 });
 
+var stylesBM = {
+  bmBurgerButton: {
+    position: 'absolute',
+    width: '18px',
+    height: '30px',
+    right: '26px',
+    top: '28px',
+  },
+  bmCrossButton: {
+    height: '32px',
+    width: '32px'
+  },
+  bmMenu: {
+    background: '#ffffff',
+    'min-width': '360px',
+  },
+  bmMorphShape: {
+    fill: '#373a47'
+  },
+  bmItemList: {
+    color: '#b8b7ad',
+  },
+  bmOverlay: {
+    background: 'rgba(0, 0, 0, 0.3)'
+  }
+};
+
 const Header = (props) => {
   if (props.type === 'home') {
     return (
-      <div className={s.container}>
-        <div className={s.header}>
-          <span>Майстерня</span>
-          <div className={s.callMenu}>
-            <div className={s.line}></div>
-            <div className={s.line}></div>
-            <div className={s.line}></div>
+      <div>
+        <div className={s.callMenu}>
+          <Menu
+            right
+            width={ '360px' }
+            customBurgerIcon={
+              <div>
+                <div className={s.line}></div>
+                <div className={s.line}></div>
+                <div className={s.line}></div>
+              </div>
+            }
+            customCrossIcon={ <img src={cross} alt="cross"/>}
+            styles={ stylesBM }>
+            <div className={s.menuItems}>
+              <Info textHeader="Майстерня" hasImage={true} menu={true}/>
+            </div>
+            <div className={s.menuFooter}>
+              <Link to="/" className={s.link}>
+                <div className={s.button}>Зв'язатись з нами</div>
+              </Link>
+            </div>
+          </Menu>
+        </div>
+        <div className={s.container}>
+          <div className={s.header}>
+            <Link to="/" className={s.link}>
+              <div className={s.headerLogo}>Майстерня</div>
+            </Link>
           </div>
         </div>
         <div className={s.logo}>
@@ -61,24 +113,46 @@ const Header = (props) => {
       </div >
     )
   } else {
+    stylesBM.bmBurgerButton.top = '34px';
     return (
-      <div className={s2.container}>
-        <div className={s2.header}>
-          <h2><Link className={s2.title} to="/">Майстерня</Link></h2>
-          <div className={s2.text}>
-            <div className={s2.text__contacts}>
-              <span className={s2.text__phone}>+38 066 445 59 00</span>
-              <span className={s2.text__place}>Доставка по Україні</span>
+      <div>
+        <div className={s2.callMenu}>
+          <Menu
+            right
+            width={ '360px' }
+            customBurgerIcon={
+              <div>
+                <div className={s.line}></div>
+                <div className={s.line}></div>
+                <div className={s.line}></div>
+              </div>
+            }
+            customCrossIcon={ <img src={cross} alt="cross"/>}
+            styles={ stylesBM }>
+            <div className={s.menuItems}>
+              <Info textHeader="Майстерня" hasImage={true} menu={true}/>
             </div>
+            <div className={s.menuFooter}>
+              <Link to="/" className={s.link}>
+                <div className={s.button}>Зв'язатись з нами</div>
+              </Link>
+            </div>
+          </Menu>
+        </div>
+        <div className={s2.container}>
+          <div className={s2.header}>
+            <h2><Link className={s2.title} to="/">Майстерня</Link></h2>
 
-            <Link to="/" classNames={s.link}>
-              <div className={s2.button}>Зв'язатись з нами</div>
-            </Link>
-            <MyLanguageSwitcher hide={true}/>
-            <div className={s2.callMenu}>
-              <div className={s2.line}></div>
-              <div className={s2.line}></div>
-              <div className={s2.line}></div>
+            <div className={s2.text}>
+              <div className={s2.text__contacts}>
+                <span className={s2.text__phone}>+38 066 445 59 00</span>
+                <span className={s2.text__place}>Доставка по Україні</span>
+              </div>
+
+              <Link to="/" classNames={s.link}>
+                <div className={s2.button}>Зв'язатись з нами</div>
+              </Link>
+              <MyLanguageSwitcher hide={true}/>
             </div>
           </div>
         </div>
